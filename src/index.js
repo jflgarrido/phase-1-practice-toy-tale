97e59e6625cb16a3cbb9440637089fb920d504cc
Julius Garrido <jflgarrido@gmail.com>
2021-12-05 10:55:44 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  //Fetch Andy's Toys
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(item => {
+      addCard(item)
+    })
+   })
+  //Show form button
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +21,55 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+  //fill out form and create toy
+  const createToyBtn = document.querySelector('.submit')
+  createToyBtn.addEventListener('click', submitToy)
+  })
+
+
+function submitToy(event){
+  console.log(event.target.image.value)
+  event.preventDefault()
+  const toyObj = {
+    name: event.target.name.value,
+    image: event.target.image.value
+  }
+  sendPost(toyObj)
+} 
+
+function addCard(item){
+  const oneCard = document.createElement('div')
+  const cardName = document.createElement('h2')
+  const cardImage = document.createElement('img')
+  const cardLikes = document.createElement('p')
+  const cardButton = document.createElement('button')
+  const cardStack = document.getElementById('toy-collection')
+  oneCard.classList.add('card')
+  cardName.innerText = item.name
+  cardImage.classList.add('toy-avatar')
+  cardImage.src = item.image
+  cardLikes.innerText = `${item.likes} `+'likes'
+  cardButton.classList.add('like-btn')
+  cardButton.setAttribute('id',`${item.id}`)
+  cardButton.innerText = 'Like'
+  cardStack.appendChild(oneCard)
+  oneCard.appendChild(cardName)
+  oneCard.appendChild(cardImage)
+  oneCard.appendChild(cardLikes)
+  oneCard.appendChild(cardButton)
+}
+
+function sendPost(newToy) {
+  fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+         Accept: 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+  .then(res => res.json())
+  .then(newToy => {
+    addCard(newToy)
+  })
+}
